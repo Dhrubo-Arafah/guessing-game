a1a463bfe276f1f8b6be9e976c645d117f9f1b05
Samiul Arafah Dhrubo <66543604+Dhrubo-Arafah@users.noreply.github.com>
2020-12-29 20:12:05 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
-document.getElementById('button').addEventListener('click', game);
+let min = document.querySelector('.min').innerHTML = '1';
+let max = document.querySelector('.max').innerHTML = '10';
 
-function game(){
-    let text = document.getElementById('text').value;
+let message = document.querySelector('.message');
+let ans = Math.floor((Math.random() * 10) + 1);
 
-    console.log(text);
+let chance = 3;
+
+let button = document.querySelector('.button');
+button.innerHTML='Check the number';
+button.addEventListener('click', game);
+
+let guess;
+
+function game(e) {
+
+    guess = parseInt(document.querySelector('.guess').value)
+
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Wrong Input! Enter a number between ${min} and ${max}`, 'red');
+    } else {
+        if (guess == ans) {
+            setMessage(`Correct Ans! You Win`, 'green');
+            lock();
+        } else {
+            chance -= 1;
+            if (chance == 0) {
+                setMessage(`You Loose! Correct Answer was ${ans}`, 'red');
+                lock();
+            } else {
+                let color;
+                if(chance == 2){
+                    color = 'orange'
+                }else{color = 'orangered'}
+                setMessage(`Wrong Input! ${chance} Chance left`, color)
+            }
+        }
+    }
+    e.preventDefault();
+}
+
+function setMessage(text, color) {
+    message.innerHTML = text;
+    message.style.backgroundColor = color;
+    document.body.style.backgroundColor = color;
+}
+
+function lock(){
+    button.innerHTML = 'Reloading Game';
+    button.style.backgroundColor = 'black';
+    document.querySelector('.guess').disabled = true;
+    setTimeout(reload, 5000);
+}
+
+function reload(){
+    location.reload();
 }
